e925a4e329558331cee7e285d9ab61f900fad6d7
msde76 <msde7648@naver.com>
2026-07-24 10:17:38 +0900

    chore: initialize identity service

---
+++ src/test/java/web/tosunsaeng/identity/IdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package web.tosunsaeng.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
